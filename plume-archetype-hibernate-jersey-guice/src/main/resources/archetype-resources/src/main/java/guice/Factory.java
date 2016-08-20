5380ac1e9f293212af22204fb6b039db1a712441
Aurélien Manteaux <amanteaux@gmail.com>
2016-08-20 20:19:17 +0200

    Externalize Guice application modules

--- plume-archetype-hibernate-jersey-guice/src/main/resources/archetype-resources/src/main/java/guice/Factory.java
+++ plume-archetype-hibernate-jersey-guice/src/main/resources/archetype-resources/src/main/java/guice/Factory.java
@@ -1,7 +1,5 @@
 package ${package}.guice;
 
-import com.coreoz.plume.conf.guice.GuiceConfModule;
-import com.coreoz.plume.jersey.guice.GuiceJacksonModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
@@ -12,10 +10,9 @@ import com.google.inject.Stage;
 public class Factory {
 	
 	private final static Injector injector = Guice.createInjector(
-			Stage.PRODUCTION,
-			new GuiceConfModule(),
-			new GuiceJacksonModule()
-		);
+		Stage.PRODUCTION,
+		new ApplicationModule()
+	);
 	
 	public final static Injector injector() {
 		return injector;
