47277893a14b93eab08df7f442516d2c0872e30b
Aurélien Manteaux <amanteaux@coreoz.com>
2017-03-20 19:26:37 +0100

    Remove unused import

--- plume-archetype-querydsl-jersey-guice-grizzly/src/main/resources/archetype-resources/src/main/java/jersey/GrizzlySetup.java
+++ plume-archetype-querydsl-jersey-guice-grizzly/src/main/resources/archetype-resources/src/main/java/jersey/GrizzlySetup.java
@@ -6,7 +6,6 @@ import java.net.URI;
 import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
-import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
