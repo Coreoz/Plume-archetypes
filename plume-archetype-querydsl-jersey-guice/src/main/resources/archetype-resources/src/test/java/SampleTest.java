7d94fc930ba131dc0a1c6d2e58eff2f81895c38d
Aurélien Manteaux <amanteaux@coreoz.com>
2023-08-28 13:26:55 +0200

    Add tests to WAR archetype

---
+++ plume-archetype-querydsl-jersey-guice/src/main/resources/archetype-resources/src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void methodToTest__test_scenario_description() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
